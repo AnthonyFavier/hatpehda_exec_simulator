a2d38a2104b7eb68a1ff72fa28c4976aeef18baf
AnthonyFavier <favier-anthony@hotmail.fr>
2023-01-24 14:28:35 +0100

    python script updated

--- rrr_bringup/script/rrr_control_python.py
+++ rrr_bringup/script/rrr_control_python.py
@@ -8,7 +8,7 @@ def my_publisher():
     # control part
 
     rospy.init_node('rrr_control_python_node')
-    control_publisher = rospy.Publisher('/arm_controller/command', JointTrajectory, queue_size=10)
+    control_publisher = rospy.Publisher('/arm_controller_bis/command', JointTrajectory, queue_size=10)
 
     while not rospy.is_shutdown():
         
@@ -16,14 +16,18 @@ def my_publisher():
 
         msg.header.stamp = rospy.Time.now()
         msg.header.frame_id = ''
-        msg.joint_names = ['joint1', 'joint2', 'joint3']
+        msg.joint_names = ['panda_joint1', 'panda_joint2', 'panda_joint3', 'panda_joint4', 'panda_joint5', 'panda_joint6', 'panda_joint7']
 
         point = JointTrajectoryPoint()
         j1 = 2 * (random.random() - 0.5)  # 0 - 1 -> -0.5 - 0.5
         j2 = 2 * (random.random() - 0.5)
         j3 = 2 * (random.random() - 0.5)
+        j4 = 2 * (random.random() - 0.5)
+        j5 = 2 * (random.random() - 0.5)
+        j6 = 2 * (random.random() - 0.5)
+        j7 = 2 * (random.random() - 0.5)
 
-        point.positions = [j1, j2, j3]
+        point.positions = [j1, j2, j3, j4, j5, j6, j7]
         point.velocities = []
         point.accelerations = []
         point.effort = []
